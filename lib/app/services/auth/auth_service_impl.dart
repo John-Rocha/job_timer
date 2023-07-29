6e3adca2c29884b4bddd300a2ad7b378824b9b87
John Rocha <jrocha@tractian.com>
2023-07-29 01:41:50 -0300

    feat: auth service implemented

---
+++ lib/app/services/auth/auth_service_impl.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import './auth_service.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthServiceImpl implements AuthService {
+  @override
+  Future<void> signIn() async {
+    final googleUser = await GoogleSignIn().signIn();
+
+    final googleAuth = await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  @override
+  Future<void> logout() async {
+    await FirebaseAuth.instance.signOut();
+    GoogleSignIn().disconnect();
+  }
+}
