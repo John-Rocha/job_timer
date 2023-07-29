b19e006d5f700bed677d9cbde83c519105703b31
John Rocha <jrocha@tractian.com>
2023-07-29 16:21:59 -0300

    feat: adds schemas

--- lib/app/core/database/database_impl.dart
+++ lib/app/core/database/database_impl.dart
@@ -1,4 +1,6 @@
 import 'package:isar/isar.dart';
+import 'package:job_timer/app/entities/project.dart';
+import 'package:job_timer/app/entities/project_task.dart';
 import 'package:path_provider/path_provider.dart';
 
 import './database.dart';
@@ -11,7 +13,10 @@ class DatabaseImpl implements Database {
     if (_databaseInstance == null) {
       final dir = await getApplicationSupportDirectory();
       _databaseInstance = await Isar.open(
-        [],
+        [
+          ProjectTaskSchema,
+          ProjectSchema,
+        ],
         directory: dir.path,
         inspector: true,
       );
